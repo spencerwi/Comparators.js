cff323ee448beed30434714280bff577dd650951
spencerwi <spencerwi@gmail.com>
2014-03-03 22:20:49 -0500

    Switching to key-extractor anonymous functions, since those are more flexible. Docco-readied the Jasmine specs.

--- comparators.spec.js
+++ comparators.spec.js
@@ -1,30 +1,43 @@
+// Comparators.js
+// ==============
 if (typeof window === "undefined"){
-    /* Running from a console spec runner in node */
+    // If we're in a console (where `window` is undefined) it's probably node, so require the module.
     Comparators = require('./comparators.js');
 }
 
 describe("Comparators", function(){
     describe(".comparing(attr|fn)", function(){
-        it("can take an attribute-name string to use as a comparator", function(){
-            var comparatorMethod = Comparators.comparing("name");
+        // Creating an "attr" comparator
+        // -----------------------------
+        it("accepts a string specifying the name of the attribute to use for comparison", function(){
             var shouldBeFirst  = {"name": "A"},
                 shouldBeSecond = {"name": "B"},
                 shouldBeThird  = {"name": "C"};
 
+            // We can specify that we want to compare our demo objects by name simply by passing
+            //  the string `"name"` to `.comparing`.
+            var comparatorMethod = Comparators.comparing("name");
+
+
             var expected   = [shouldBeFirst, shouldBeSecond, shouldBeThird];
             var beforeSort = [shouldBeSecond, shouldBeFirst, shouldBeThird];
             var actual = beforeSort.sort(comparatorMethod);
+
             expect(actual).toEqual(expected);
         });
-        it("can take a compareTo-style function to use as a comparator", function(){
-            var comparatorMethod = Comparators.comparing(function(first, second){
-                if      (first.name.length > second.name.length) { return  1; }
-                else if (first.name.length < second.name.length) { return -1; }
-                else { return 0; }
-            });
+        // Creating a "fn" comparator
+        // --------------------------
+        it("accepts an 'attribute-extractor' function that returns the attribute to use for comparison", function(){
             var shouldBeFirst  = {"name": "short"},
                 shouldBeSecond = {"name": "pretty long"},
                 shouldBeThird  = {"name": "definitely the longest"};
+
+            // If the comparison key is a calculated value (or something that's not a direct property of each object in the array),
+            //  we can create a chainable comparator that generates the comparison key using an anonymous function.
+            // In this example, we want to sort by name length (largest to smallest)
+            var comparatorMethod = Comparators.comparing(function(each){
+                return each.name.length;
+            });
 
             var expected   = [shouldBeFirst, shouldBeSecond, shouldBeThird];
             var beforeSort = [shouldBeSecond, shouldBeFirst, shouldBeThird];
@@ -34,7 +47,14 @@ describe("Comparators", function(){
     });
 
     describe(".thenComparing(attr|fn)", function(){
+        // Chaining with an "attr" comparator
+        // ----------------------------------
         it("chains from '.comparing(attr|fn)' and accepts an attribute-name string", function(){
+            // As with `.comparing(attr)`, we can specify a comparison-key attribute just by passing in the
+            //  attribute name as a string.
+            // The advantage of `thenComparing(attr)` is that it lets us easily define a hierarchical multi-key sort.
+            //
+            // In this case, we sort alphabetically by lastName, then by firstName -- like an old phone book.
             var comparatorMethod = Comparators.comparing("lastName").thenComparing("firstName");
             var shouldBeFirst  = {"firstName": "A", "lastName": "A"},
                 shouldBeSecond = {"firstName": "B", "lastName": "A"},
@@ -45,11 +65,16 @@ describe("Comparators", function(){
             var actual = beforeSort.sort(comparatorMethod);
             expect(actual).toEqual(expected);
         });
-        it("chains from '.comparing(attr|fn)' and accepts a compareTo-style function", function(){
-            var comparatorMethod = Comparators.comparing("lastName").thenComparing(function(first, second){
-                if      (first.firstName.length > second.firstName.length){ return  1; }
-                else if (first.firstName.length < second.firstName.length){ return -1; }
-                else { return 0; }
+        // Chaining with a "fn" comparator
+        // -------------------------------
+        it("chains from '.comparing(attr|fn)' and accepts an attribute-extractor function", function(){
+            // And as with `.comparing(fn)`, we can pass in a function that returns the comparison key
+            //  when given each element of the method as a parameter. Functors are neat!
+            //
+            // This example also demonstrates that `(attr)` and `(fn)` comparators can be mixed-and-matched
+            //  to produce the desired results.
+            var comparatorMethod = Comparators.comparing("lastName").thenComparing(function(each){
+                return each.firstName.length;
             });
             var shouldBeFirst  = {"firstName": "short",             "lastName": "A"},
                 shouldBeSecond = {"firstName": "Rather long",       "lastName": "A"},
