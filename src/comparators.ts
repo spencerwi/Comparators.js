d54a3d9c5432c3034c28cd58c7de05e890930d8e
Spencer Williams <spencerwi@gmail.com>
2016-02-16 12:36:41 -0500

    Rewrite in Typescript, bundle a definition file, switch to painless for testing

---
+++ src/comparators.ts
@@ -0,0 +1,54 @@
+export interface KeyExtractorFn<T> {
+    (t: T): any;
+}
+
+export interface ComparatorOptions {
+    reversed?: boolean;
+}
+
+export interface Comparator<T> {
+    (a: T, b: T): number;
+    thenComparing?(attrOrFunction: string|KeyExtractorFn<T>, opts?: ComparatorOptions): Comparator<T>;
+}
+interface ComparatorInternal<T> extends Comparator<T> {
+    nextStep?: Comparator<T>;
+}
+
+var buildComparisonStep = < T >(attrOrFunction: string|KeyExtractorFn<T>, opts:ComparatorOptions): Comparator<T> => {
+    const reversed: boolean = (opts && opts.reversed);
+    var comparatorFunction: ComparatorInternal<T> = (firstItem: T, secondItem: T): number => {
+        let comparisonValueOfFirstItem: any;
+        let comparisonValueOfSecondItem: any;
+        let result: number;
+        if (typeof attrOrFunction === "function"){
+            comparisonValueOfFirstItem  = attrOrFunction(firstItem);
+            comparisonValueOfSecondItem = attrOrFunction(secondItem);
+        } else {
+            comparisonValueOfFirstItem = firstItem[attrOrFunction];
+            comparisonValueOfSecondItem = secondItem[attrOrFunction];
+        }
+        if (comparisonValueOfFirstItem > comparisonValueOfSecondItem) {
+            if (reversed) { result = -1; }
+            else          { result = 1;  }
+        } else if (comparisonValueOfFirstItem < comparisonValueOfSecondItem) {
+            if (reversed){ result =  1;}
+            else         { result = -1; }
+        } else {
+            if (comparatorFunction.nextStep != undefined) { result = comparatorFunction.nextStep(firstItem, secondItem); }
+            else { result = 0; }
+        }
+        return result;
+    }
+    let lastStepInComparisonChain = comparatorFunction;
+    comparatorFunction.thenComparing = function(attrOrFunction, opts) {
+        lastStepInComparisonChain = lastStepInComparisonChain.nextStep = buildComparisonStep(attrOrFunction, opts);
+        return this;
+    }
+    return comparatorFunction;
+}
+
+export default class Comparators {
+    public static comparing<T>(attrOrFunction: KeyExtractorFn<T>, opts?: ComparatorOptions) {
+        return buildComparisonStep<T>(attrOrFunction, opts);
+    }
+}
